cd0c687f74348233750fc7ac15ef5d03034ffe4b
alainfranck <franckalain.dev@gmail.com>
2024-02-17 13:01:41 +0000

    allowed_host

--- portail_economie/settings.py
+++ portail_economie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4n$q^+xg-lhwdw9@ccc&$(z*=12yhl6!g^q=6k!b7&nxr#k+83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
